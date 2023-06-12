2405f0731d86119a0048a7475af2c9f570214a5b
Saba <phruidzesaba@gmail.com>
2023-06-12 12:26:51 +0400

    I think I made most of the logic part I will check it once more and then I will start the visual

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer } from "react";
+import "./App.css";
+
+type initialState = {
+  bill: number;
+  tip: number;
+  nop: number; //number of people
+};
+type State = typeof initialState;
+type actionType = {
+  type: string;
+  payload: number;
+};
+const initialState: initialState = {
+  bill: 0,
+  tip: 0,
+  nop: 0,
+};
+const reducer = (state: State, action: actionType) => {
+  let change = { ...state };
+  switch (action.type) {
+    case "GetBillValue":
+      change.bill = action.payload;
+      return change;
+      break;
+    case "GetTipValue":
+      change.tip = action.payload;
+      return change;
+      break;
+    case "GetNOP":
+      change.nop = action.payload;
+      return change;
+      break;
+    default:
+      return change;
+  }
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [start, dispatch] = useReducer(reducer, initialState);
+  function dispatchUse(type: string, payload: number) {
+    dispatch({
+      type: type,
+      payload: payload,
+    });
+  }
+  const allRight =
+    start.bill !== null && start.tip !== null && start.nop !== null;
 
+  const TipCalculation =
+    allRight && ((start.bill * start.tip) / start.nop).toFixed(2);
+  const total =
+    allRight && ((start.bill * (1 + start.tip)) / start.nop).toFixed(2);
+
+  const showTotal = !(TipCalculation === "NaN");
+  const showTip = !(TipCalculation === "NaN");
   return (
-    <>
+    <div>
+      <input
+        type="number"
+        value={start.bill}
+        onChange={(e) => {
+          const value = e.target.valueAsNumber;
+          dispatchUse("GetBillValue", value);
+        }}
+      />
+      <p>{start.bill}</p>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div
+          onClick={() => {
+            const value = 0.05;
+            dispatchUse("GetTipValue", value);
+          }}
+        >
+          5%
+        </div>
+        <p>{start.tip}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <input
+        type="number"
+        value={start.nop}
+        onChange={(e) => {
+          const value = e.target.valueAsNumber;
+          dispatchUse("GetNOP", value);
+        }}
+      />
+      <p>{start.nop}</p>
+
+      <div>
+        {showTip ? TipCalculation : "0.00"}
+        <br />
+        {showTotal ? total : "0.00"}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
